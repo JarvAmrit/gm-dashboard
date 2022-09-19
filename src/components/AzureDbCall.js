e3082a7f1aec19f887ecd516e096ddeb9ca3b1e3
JarvAmrit <amritkr1994@gmail.com>
2022-09-20 02:45:23 +0530

    Initial Push9.

--- src/components/AzureDbCall.js
+++ src/components/AzureDbCall.js
@@ -3,20 +3,21 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 
-const [userData,setuserData]=useState([]);
-const getuserData= ()=>{    
-    axios.get('https://reactapi-app.azurewebsites.net/api/getlist?code=3N0GbtuQTIBuXsygkLg50Dg5r9ZJewT4o7duvfDggJEtAzFuqI1bUQ==')
-    .then((res)=>{
-        setuserData(res.data)
-    })
-}
-
-useEffect(()=>{
-getuserData();
-},[])
-
-
 export default function AzureDbCall() {
+
+    const [userData,setuserData]=useState([]);
+
+    const getuserData= ()=>{    
+        axios.get('https://reactapi-app.azurewebsites.net/api/getlist?code=3N0GbtuQTIBuXsygkLg50Dg5r9ZJewT4o7duvfDggJEtAzFuqI1bUQ==')
+        .then((res)=>{
+            setuserData(res.data)
+        })
+    }
+
+    useEffect(()=>{
+        getuserData();
+        },[])
+        
 
       return (
         <div>
