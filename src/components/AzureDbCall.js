f942b726e4d99ab9cad9d64cf2422e49830bbdd4
JarvAmrit <amritkr1994@gmail.com>
2022-09-20 03:37:57 +0530

    Initial Push11.

--- src/components/AzureDbCall.js
+++ src/components/AzureDbCall.js
@@ -1,36 +1,52 @@
-
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-
+import axios from "axios";
 
 export default function AzureDbCall() {
+  const [userData, setuserData] = useState([]);
 
-    const [userData,setuserData]=useState([]);
+  const getuserData = () => {
+    axios
+      .get(
+        "https://reactapi-app.azurewebsites.net/api/getlist?code=3N0GbtuQTIBuXsygkLg5QDg5r9ZJewT4o7duvfDggJEtAzFuqI1bUQ=="
+      )
+      .then((res) => {
+        setuserData(res.data);
+      });
+  };
 
-    const getuserData= ()=>{    
-        axios.get('https://reactapi-app.azurewebsites.net/api/getlist?code=3N0GbtuQTIBuXsygkLg5QDg5r9ZJewT4o7duvfDggJEtAzFuqI1bUQ==')
-        .then((res)=>{
-            setuserData(res.data)
-        })
-    }
+  useEffect(() => {
+    getuserData();
+  }, []);
 
-    useEffect(()=>{
-        getuserData();
-        },[])
-        
-
-      return (
-        <div>
-            <>            
-            {userData && userData.map((data)=>{
-                <>
-                    <h3>data.id</h3>
-                    </>
-            })}
-            </>
-        </div>
-        
-      );
-    
-  }
-  
+  return (
+    <div>
+        <table id="customers">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Occupation</th>
+                    <th>Hobbie</th>
+                    <th>Id</th>
+                    
+                </tr>
+            </thead>
+        </table>
+      {userData &&
+        userData.map((data) => {
+          return (
+            <table id="customers">
+              
+              <tbody>
+               <tr>
+                <td>{data.name}</td>
+                <td>{data.occupation}</td>
+                <td>{data.hobbie}</td>
+                <td>{data._id}</td>
+                </tr>
+              </tbody>
+            </table>
+          );
+        })}
+    </div>
+  );
+}
